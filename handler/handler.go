fc0b04e5255d0d99499d52491a328b5450296981
阿超 <fc277073030@users.noreply.github.com>
2020-02-12 00:21:59 +0800

    update

--- handler/handler.go
+++ handler/handler.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-func UploadHandler(w http.ResponseWriter, r *http.Request)  {
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		//返回上传html页面
 		data, err := ioutil.ReadFile("./static/view/index.html")
@@ -17,7 +17,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request)  {
 			return
 		}
 		io.WriteString(w, string(data))
-	}else if r.Method == "POST" {
+	} else if r.Method == "POST" {
 		//接收文件流及存储到本地目录
 		file, head, err := r.FormFile("file")
 		if err != nil {
@@ -26,23 +26,23 @@ func UploadHandler(w http.ResponseWriter, r *http.Request)  {
 		}
 		defer file.Close()
 
-		newFile, err := os.Create("/tmp"+head.Filename)
+		newFile, err := os.Create("./uploads" + head.Filename)
 		if err != nil {
-			fmt.Printf("Fail to create file, err: %s\n",err.Error())
+			fmt.Printf("Fail to create file, err: %s\n", err.Error())
 			return
 		}
 		defer newFile.Close()
 
 		_, err = io.Copy(newFile, file)
 		if err != nil {
-			fmt.Printf("Failed to save data into file, err:%s\n",err.Error())
+			fmt.Printf("Failed to save data into file, err:%s\n", err.Error())
 			return
 		}
 		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
 }
 
-func UploadSucHandler(w http.ResponseWriter, r *http.Request)  {
+func UploadSucHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Upload finished!")
 
 }
